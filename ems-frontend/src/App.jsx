9d5d1c9b15454236766a252348bad39294b1b186
Nidhi Kumari <nsinha7295@gmail.com>
2025-04-21 17:35:38 -0400

    useState Hook

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import HelloWorld from './HelloWorld'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <HelloWorld />
+      <ListEmployeeComponent />
     </>
   )
 }
