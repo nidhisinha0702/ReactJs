3b6e6eaf5f47b5afefedc7d11bfb1d6acde75c2e
Nidhi Kumari <nsinha7295@gmail.com>
2025-04-22 17:16:41 -0400

    add-employee

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-import HelloWorld from './HelloWorld'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import EmployeeComponent from './components/EmployeeComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <ListEmployeeComponent />
+    <BrowserRouter>
+      <HeaderComponent />
+      <Routes>
+        {/* http://localhost:3000 */}
+        <Route path='/' element={<ListEmployeeComponent />}></Route>
+         {/* http://localhost:3000/employees */}
+        <Route path='/employees' element={<ListEmployeeComponent />}></Route>
+         {/* http://localhost:3000/add-employee */}
+        <Route path='/add-employee' element={<EmployeeComponent/>}></Route>
+      </Routes>
+      <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className='navbar navbar-dark bg-dark'></nav>
+        <a className="navbar-brand" href="https://www.google.com">Employee Management System</a>
+      </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
